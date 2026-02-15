dae84d051ad41125ab85b1dd80705de714c09e90
Herasymenko Viktor <vg706638@gmail.com>
2026-02-15 17:27:47 +0200

    Api is connected to telegram

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,17 @@
 using Scalar.AspNetCore;
+using Telegram.Bot.Types;
+using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddLogging();
+
 // Add services to the container.
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.MaxDepth = 1024;
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -15,7 +24,12 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/health", () => "Health OK");
 
-app.MapGet("/health", () => "OK");
+app.MapPost("/", (Update update, ILogger<Program> logger) => 
+{
+    logger.LogInformation("Отримано текст: {Text}", update.Message?.Text ?? "No text");
+    return Results.Ok("Bot is working");
+});
+    
 app.Run();
